7f3434f67bd4814cebf65d0f82fbb7b6e51f62dc
Weiling <yanweiling29@gmail.com>
2020-07-21 18:20:17 -0400

    7/21/2020

--- CSE248LibraryApp/src/sample/Name.java
+++ CSE248LibraryApp/src/sample/Name.java
@@ -5,13 +5,12 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Name {
-	
+
 	private String firstName;
 	private String lastName;
-	private String name;	
-	
+	private String name;
+
 	public Name() throws FileNotFoundException {
 		super();
 		this.firstName = setFirstName();
@@ -25,18 +24,18 @@ public class Name {
 
 	public String getLastName() {
 		return lastName;
-	}	
-	
-	public String getName(){		
-		return name;		
 	}
-	
+
+	public String getName() {
+		return name;
+	}
+
 	private String setName() {
 		name = firstName + " " + lastName;
 		return name;
 	}
 
-	private String setLastName() throws FileNotFoundException{
+	private String setLastName() throws FileNotFoundException {
 		int count = 0;
 		String temp;
 		Scanner scanner = new Scanner(new FileInputStream("Last Names.txt"));
@@ -52,8 +51,8 @@ public class Name {
 		temp = lNameList.get(random).toString();
 		return temp;
 	}
-	
-	private String setFirstName() throws FileNotFoundException{
+
+	private String setFirstName() throws FileNotFoundException {
 		int count = 0;
 		String temp;
 		Scanner scanner = new Scanner(new FileInputStream("First Names.txt"));
@@ -69,13 +68,10 @@ public class Name {
 		temp = fNameList.get(random).toString();
 		return temp;
 	}
-		
-	
-
 
 	@Override
 	public String toString() {
-		return "name=" + name ;
+		return "name=" + name;
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -87,104 +83,5 @@ public class Name {
 		System.out.println(names);
 
 	}
-	
+
 }
-
-<<<<<<< HEAD
-
-=======
-=======
-package sample;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-
-public class Name {
-	
-	private String firstName;
-	private String lastName;
-	private String name;	
-	
-	public Name() throws FileNotFoundException {
-		super();
-		this.firstName = setFirstName();
-		this.lastName = setLastName();
-		this.name = setName();
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}	
-	
-	public String getName(){		
-		return name;		
-	}
-	
-	private String setName() {
-		name = firstName + " " + lastName;
-		return name;
-	}
-
-	private String setLastName() throws FileNotFoundException{
-		int count = 0;
-		String temp;
-		Scanner scanner = new Scanner(new FileInputStream("Last Names.txt"));
-		ArrayList<String> lNameList = new ArrayList<>();
-		while (scanner.hasNextLine()) {
-			String line = scanner.nextLine();
-			count++;
-			lNameList.add(line);
-		}
-		int min = 0;
-		int max = count;
-		int random = (int) (Math.random() * (max - min + 1) + min);
-		temp = lNameList.get(random).toString();
-		return temp;
-	}
-	
-	private String setFirstName() throws FileNotFoundException{
-		int count = 0;
-		String temp;
-		Scanner scanner = new Scanner(new FileInputStream("First Names.txt"));
-		ArrayList<String> fNameList = new ArrayList<>();
-		while (scanner.hasNextLine()) {
-			String line = scanner.nextLine();
-			count++;
-			fNameList.add(line);
-		}
-		int min = 0;
-		int max = count;
-		int random = (int) (Math.random() * (max - min + 1) + min);
-		temp = fNameList.get(random).toString();
-		return temp;
-	}
-		
-	
-
-
-	@Override
-	public String toString() {
-		return "name=" + name ;
-	}
-
-	public static void main(String[] args) throws FileNotFoundException {
-		ArrayList<Name> names = new ArrayList<>();
-		for (int i = 0; i < 10; i++) {
-			Name a = new Name();
-			names.add(a);
-		}
-		System.out.println(names);
-
-	}
-	
-}
-
->>>>>>> 8b43c08878d7e1fae1b3e48719cf58391da893d3:CSE248LibraryApp/src/p1/Name.java
->>>>>>> refs/remotes/origin/master
